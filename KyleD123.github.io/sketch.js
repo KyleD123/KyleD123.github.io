28822a3ff84ab0ab789af14e47646ef7fba5de07
KyleD123 <Kyledoerksen75@yahoo.ca>
2020-02-06 14:09:57 -0600

    Interactive Scene
    I made grass, a sky, a guy, and a girl so far.

--- KyleD123.github.io/sketch.js
+++ KyleD123.github.io/sketch.js
@@ -1,15 +1,28 @@
 // Project Title
-// Your Name
+// Kyle Doerksen
 // Date
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let r = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  print(frameCount);
+  background(0);
+  fill(r,0,0)
+  ellipse(mouseX,mouseY,40,40);
+  
 }
+
+function mousePressed(){
+    if (r === 0) 
+      r = 255;
+    }
+    else{
+      r = 0;
+    }
